b28dc63d231dce091bea4c66cb9a6b816915c933
Crystal Tai <crystaltai.xyz@gmail.com>
2023-09-23 13:05:31 -0400

    setup thirdweb, styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+import '@/styles/globals.css';
+
+const activeChain = 'mumbai';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider activeChain={activeChain}>
+      <main className={poppins.variable}>
+        <Component {...pageProps} />
+      </main>
+    </ThirdwebProvider>
+  );
 }
+
+// Import fonts
+import { Poppins } from 'next/font/google';
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--font-poppins',
+});
